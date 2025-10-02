213571ecbcf98bf184246c3482d9a4294048c174
zucoloto <zucoloto.alessandro@gmail.com>
2025-10-02 20:58:21 -0300

    Inserindo departamento

--- src/main/java/br/com/zuco/controller/DepartamentoController.java
+++ src/main/java/br/com/zuco/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package br.com.zuco.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.zuco.entity.Departamento;
+import br.com.zuco.service.DepartamentoService;
 
 @Controller
 @RequestMapping("departamentos")
 public class DepartamentoController {
 
+	@Autowired
+	DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
@@ -17,4 +25,11 @@ public class DepartamentoController {
 	public String listar() {
 		return "/departamento/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
 }
