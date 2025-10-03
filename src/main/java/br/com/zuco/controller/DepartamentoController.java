09ad635cb684f1710bb41dde5770133d274480c0
zucoloto <zucoloto.alessandro@gmail.com>
2025-10-03 11:47:48 -0300

    Mensagem de Alerta

--- src/main/java/br/com/zuco/controller/DepartamentoController.java
+++ src/main/java/br/com/zuco/controller/DepartamentoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.zuco.entity.Departamento;
 import br.com.zuco.service.DepartamentoService;
@@ -30,8 +31,9 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Departamento departamento) {
+	public String salvar(Departamento departamento, RedirectAttributes attr) {
 		service.salvar(departamento);
+		attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
 		return "redirect:/departamentos/cadastrar";
 	}
 	
@@ -42,16 +44,21 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/atualizar")
-	public  String atualizar(Departamento departamento) {
+	public  String atualizar(Departamento departamento, RedirectAttributes attr) {
 		service.atualizar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 		return "redirect:/departamentos/cadastrar";
 	}
 	
 	@GetMapping("/excluir/{id}")
 	public String excluir(@PathVariable("id") Long id1, ModelMap model) {
-		if (!service.departamentoTemCargos(id1)) {
+		if (service.departamentoTemCargos(id1)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargo(s) vinculado(s).");
+		} else {
 			service.excluir(id1);
+			model.addAttribute("success", "Departamento excluído com sucesso.");
 		}
+		
 		return listar(model);
 	}
 	
