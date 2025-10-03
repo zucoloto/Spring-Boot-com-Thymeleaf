9f64749d23c5c4d88d99e74d3a691badacf30333
zucoloto <zucoloto.alessandro@gmail.com>
2025-10-03 19:57:51 -0300

    Listando Cargo

--- src/main/java/br/com/zuco/controller/CargoController.java
+++ src/main/java/br/com/zuco/controller/CargoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -32,7 +33,8 @@ public class CargoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista";
 	}
 	
